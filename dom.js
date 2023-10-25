977a97235b8e7faf95fbab9771d6453bc30826d2
nasimpcm <nasimpcm@gmail.com>
2023-10-25 11:07:11 +0530

    sharpener 3

--- dom.js
+++ dom.js
@@ -22,13 +22,14 @@
 // var addItem1 = document.getElementById('additems1');
 // addItem1.style.color = 'green';
 // addItem1.style.fontWeight = 'bold'; 
-var items = document.getElementsByClassName('list-group-item');
+var items = document.getElementsByTagName('li');
+//var items = document.getElementsByClassName('list-group-item');
 items[1].textContent = 'Hello 2';
-// items[0].style.fontWeight = 'bold';
+//items[0].style.fontWeight = 'bold';
 // items[1].style.fontWeight = 'bold';
 // items[2].style.fontWeight = 'bold';
 // items[3].style.fontWeight = 'bold';
 items[2].style.backgroundColor = 'green';
-for (var i = 0; i < items; i++) {
+for (var i = 0; i < items.length; i++) {
     items[i].style.fontWeight = 'bold';
 }
